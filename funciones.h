cba843ed2c3adaf14482d783389def0144bfa3e6
CarlosGprogra <carlos.gomez.hermosa@udla.edu.ec>
2025-01-10 08:47:59 -0500

    Update funciones.h

--- funciones.h
+++ funciones.h
@@ -1,22 +1,29 @@
-struct Producto 
-{
+#ifndef FUNCIONES_H
+#define FUNCIONES_H
+
+#define MAX_PRODUCTOS 5
+
+struct Producto {
     char nombre[50];
     int cantidad;
     float precio;
 };
 
-struct Factura
-{
+struct Factura {
     char nombre[20];
     int cedula;
-    int numProductos; 
-    float total; 
-    struct Producto productos[5]; 
-}; 
-int menu ();
-void savefactura (struct Factura *factura); 
-void createFactura (); 
-void leerCadena (char *cadena,int num); 
-void readFactura ();
+    int numProductos;
+    float total;
+    struct Producto productos[MAX_PRODUCTOS];
+};
+
+int menu();
+void savefactura(struct Factura *factura);
+void createFactura();
+void leerCadena(char *cadena, int num);
+void readFactura();
 void updateFactura();
 void deleteFactura();
+void mostrarFacturaDetallada();
+
+#endif 
